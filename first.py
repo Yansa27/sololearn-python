8d3747c0c38d844f704b43c73eb02c2370176b25
Yansa27 <juliansa837@gmail.com>
2022-10-16 23:21:09 +0700

    day one solo learn

---
+++ first.py
@@ -0,0 +1,6 @@
+#SOLO LEARN SIMPLE CALCULATION (PYTHON)
+# You need to calculate the points earned by a soccer team.
+#The team won 18 games and ended 7 games as a draw.
+#A win brings 3 points, while a draw brings 1.
+
+print(18*3+7)
---
+++ learn.py
@@ -0,0 +1,8 @@
+def main ():
+    for i in range (1, 11):
+        for j in range (1, i+1):
+            print ("%d " % (i+j), end='')
+        print()
+if __name__ == "__main__" :
+    main()            
+
