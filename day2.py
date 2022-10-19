b44ced73d9096cf7e18be834af1559c53571d79c
Yansa27 <juliansa837@gmail.com>
2022-10-19 22:47:00 +0700

    day2 hehe

---
+++ day2.py
@@ -0,0 +1,14 @@
+print(7% (5//2))
+print(6//1.5)
+
+#Write code to output 4 raised to the 5th power.
+print(4**5)
+
+print((3**2)//2)
+
+
+#You need to calculate the flight time of an upcoming trip. You are flying from LA to Sydney, covering a distance of 7425 miles, the plane flies at an average speed of 550 miles an hour.
+#Calculate and output the total flight time in hours.
+#Hint
+#The result should be a float.
+print (float(7425/550))
