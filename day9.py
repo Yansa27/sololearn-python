31b712617d16b848c527df65459254152083ffa2
Yansa27 <juliansa837@gmail.com>
2022-10-31 21:52:38 +0700

    push

---
+++ day9.py
@@ -0,0 +1,10 @@
+nama = input('masukkan nama anda : ')
+nim = input('masukkan nim anda : ')
+nilai_absensi = input('masukkan nilai absensi anda : ')
+nilai_quis = input('masukkan nilai quis anda : ')
+nilai_tugas = input('masukkan nilai tugas anda : ')
+nilai_mid = input('masukkan nilai mid anda : ')
+nilai_uas = input('masukkan nilai uas anda : ')
+nilai_huruf = input('masukkan nilai huruf anda : ')
+nilai_akhir = 1
+
