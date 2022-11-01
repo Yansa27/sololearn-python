e2b21b199bba92725329933bde70b5d05d5b6875
Yansa27 <juliansa837@gmail.com>
2022-11-01 20:21:42 +0700

    belum selesai

--- day9.py
+++ day9.py
@@ -1,10 +1,29 @@
+#50%
 nama = input('masukkan nama anda : ')
 nim = input('masukkan nim anda : ')
-nilai_absensi = input('masukkan nilai absensi anda : ')
-nilai_quis = input('masukkan nilai quis anda : ')
-nilai_tugas = input('masukkan nilai tugas anda : ')
-nilai_mid = input('masukkan nilai mid anda : ')
-nilai_uas = input('masukkan nilai uas anda : ')
+nilai_absensi = eval(input('masukkan nilai absensi anda : '))
+nilai_quis = eval(input('masukkan nilai quis anda : '))
+nilai_tugas = eval(input('masukkan nilai tugas anda : '))
+nilai_mid = eval(input('masukkan nilai mid anda : '))
+nilai_uas = eval(input('masukkan nilai uas anda : '))
 nilai_huruf = input('masukkan nilai huruf anda : ')
-nilai_akhir = 1
+nilai_huruf = int(nilai_huruf)
+nilai_akhir = nilai_absensi + nilai_quis + nilai_tugas + nilai_mid + nilai_uas 
+nilai_akhir = int(nilai_akhir)
+total_nilai = nilai_akhir / 5
+print(total_nilai)
+if  nilai_huruf >= 90 :
+    nilai_huruf = 'A'
+elif nilai_huruf >= 80 :
+    nilai_huruf = 'B'   
+elif nilai_huruf >= 70  :
+    nilai_huruf = 'C'
+elif nilai_huruf >= 60 :
+    nilai_huruf = 'D'  
+elif nilai_huruf < 60 :
+    nilai_huruf = 'E'      
+
+print(f'{nilai_huruf}')    
+
+
 
