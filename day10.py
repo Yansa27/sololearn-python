5dfb61585215c16c2a6ffe85edfdb3ff84d42764
Yansa27 <juliansa837@gmail.com>
2022-11-01 20:57:26 +0700

    day 10

---
+++ day10.py
@@ -0,0 +1,11 @@
+jawab = input('jawab iya/tidak : ')
+hitung = 0
+while jawab != 'iya' :
+    print('jalan')
+    hitung += 1
+    if hitung >= 5:
+        break
+print(f'total print {hitung}')    
+
+
+
