2e9c6b63288165e325531727f5d038be77b82f29
Yansa27 <juliansa837@gmail.com>
2022-11-06 23:44:38 +0700

    push

--- day11.py
+++ day11.py
@@ -1,11 +1,10 @@
 name = 'yansa'
 x = 0
 for i in name :
-    x += 1 
+    x += 1
     print(name [0:x])
     
 for i in name : 
-    x -= 1 
+    x -= 1
     print(name [0:x])    
 
-   
---
+++ day12.py
@@ -0,0 +1,13 @@
+name = input('masukkan nama anda : ')
+
+while name != 'yansa' :
+    print('password salah')
+    
+    if name != 'yansa' :
+        break
+
+    if name == 'yansa' :
+        print(f'Selamat datang {name}')
+
+
+
