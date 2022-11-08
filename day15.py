5d3a9da9b37d8a0bb3a3dbe32ac9ad088d2ad2fe
Yansa27 <juliansa837@gmail.com>
2022-11-09 00:01:50 +0700

    day 15

---
+++ day15.py
@@ -0,0 +1,17 @@
+# list day 15 
+
+name =["yansa" , "apa" , "tes" , "nalar" , "yakin"]
+
+for item in name :
+    print(f'nama : {item}')
+
+
+#list method 
+numbers = [1, 2 , 5 , 8]
+print(numbers)
+
+numbers.append(int(input('masukkan angka : ')))
+print(numbers)
+
+numbers.insert(2, 10)
+print(numbers)
