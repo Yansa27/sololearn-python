44d6b5f52398452f4551947b99b9706888ac4733
Yansa27 <juliansa837@gmail.com>
2022-11-10 22:36:20 +0700

    push

---
+++ day16.py
@@ -0,0 +1,20 @@
+# list method 2
+name = ['yansa' , 'siapa' , 'hallodeck' , 'siapa ya']
+print(name)
+
+for names in name :
+    print(f'nama : {names}')
+name.append('kon')
+print(name)
+
+
+numbers = [1 , 2 , 3 , 4 ,5 , 6]
+print(numbers)
+
+numbers.insert(2, 30)
+print(numbers)
+
+for number in numbers :
+    numbers.append(input('masukkan : '))
+    print(numbers)
+
